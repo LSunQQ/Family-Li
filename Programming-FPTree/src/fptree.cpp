a7157b10593911c8c2fbb7591d36400fb64deaa0
mmdxm <34512530+mmdxm@users.noreply.github.com>
2019-05-18 22:21:02 +0800

    Update fptree.cpp

--- Programming-FPTree/src/fptree.cpp
+++ Programming-FPTree/src/fptree.cpp
@@ -102,3 +102,197 @@ KeyNode* InnerNode::insert(const Key& k, const Value& v) {
     }
     return newChild;
 }
+
+// ensure that the leaves inserted are ordered
+// used by the bulkLoading func
+// inserted data: | minKey of leaf | LeafNode* |
+KeyNode* InnerNode::insertLeaf(const KeyNode& leaf) {
+    KeyNode* newChild = NULL;
+    // first and second leaf insertion into the tree
+    if (this->isRoot && this->nKeys == 0 && this->nChild==0) {
+        // TODO
+        this->keys[0] = leaf.key;
+        this->childrens[1] = leaf.node;
+        LeafNode * newnode = new LeafNode(tree);
+        this->childrens[0] = newnode;
+        this->nChild = 2;
+        this->nKeys = 1;
+        return NULL;
+    }
+    
+    // recursive insert
+    // Tip: please judge whether this InnerNode is full
+    // next level is not leaf, just insertLeaf
+    // TODO
+
+    // next level is leaf, insert to childrens array
+    // TODO
+    int place = findIndex(leaf.key);
+    if (this->childrens[0]->ifLeaf()) {
+        this->insertNonFull(leaf.key, leaf.node);
+        if (this->nKeys == 2 * this->degree + 1) {
+            KeyNode * newInnernode = this->split();
+            if(this->isRoot) {
+                InnerNode *newroot = new InnerNode(this->degree,this->tree,true);
+                this->isRoot = 0;
+                newroot->childrens[0] = this;
+                newroot->nChild = 2;
+                newroot->insertNonFull(newInnernode->key,newInnernode->node);
+                newroot->nKeys = 1;
+                this->tree->changeRoot(newroot);
+                return NULL;
+            }
+            else return newInnernode;
+        }
+        else return NULL;
+    }
+    else{
+        newChild = ((InnerNode*)this->childrens[place])->insertLeaf(leaf);
+        if(newChild == NULL) return newChild;
+        else {
+            insertNonFull(newChild->key,newChild->node);
+            if (this->nKeys == 2 * this->degree + 1) {
+                KeyNode * newInnernode = this->split();
+                if(this->isRoot) {
+                    InnerNode *newroot = new InnerNode(this->degree,this->tree,true);
+                    this->isRoot = 0;
+                    newroot->childrens[0] = this;
+                    newroot->nChild = 2;
+                    newroot->insertNonFull(newInnernode->key,newInnernode->node);
+                    newroot->nKeys = 1;
+                    this->tree->changeRoot(newroot);
+                    return NULL;
+                }
+                else return newInnernode;
+            }
+            else return NULL;
+        }
+    }
+    return newChild;
+}
+
+KeyNode* InnerNode::split() {
+    KeyNode* newChild = new KeyNode();
+    // right half entries of old node to the new node, others to the old node. 
+    // TODO
+    newChild->key = this->keys[this->degree];
+    InnerNode *temp = new InnerNode(this->degree, this->tree, false);
+    temp->nKeys = this->degree;
+    temp->nChild = this->degree + 1;
+    for (int i = 0; i <= this->degree; ++i) {
+        if (i < this->degree)
+            temp->keys[i] = this->keys[this->degree + i + 1];
+        temp->childrens[i] = this->childrens[this->degree + i + 1];
+    }
+    this->nChild /= 2;
+    this->nKeys /= 2;
+    newChild->node = temp;
+    return newChild;
+}
+
+// remove the target entry
+// return TRUE if the children node is deleted after removement.
+// the InnerNode need to be redistributed or merged after deleting one of its children node.
+bool InnerNode::remove(const Key& k, const int& index, InnerNode* const& parent, bool &ifDelete) {
+    bool ifRemove = false;
+    // only have one leaf
+    // TODO
+    
+    // recursive remove
+    // TODO
+    return ifRemove;
+}
+
+// If the leftBro and rightBro exist, the rightBro is prior to be used
+void InnerNode::getBrother(const int& index, InnerNode* const& parent, InnerNode* &leftBro, InnerNode* &rightBro) {
+    // TODO
+}
+
+// merge this node, its parent and left brother(parent is root)
+void InnerNode::mergeParentLeft(InnerNode* const& parent, InnerNode* const& leftBro) {
+    // TODO
+}
+
+// merge this node, its parent and right brother(parent is root)
+void InnerNode::mergeParentRight(InnerNode* const& parent, InnerNode* const& rightBro) {
+    // TODO
+}
+
+// this node and its left brother redistribute
+// the left has more entries
+void InnerNode::redistributeLeft(const int& index, InnerNode* const& leftBro, InnerNode* const& parent) {
+    // TODO
+}
+
+// this node and its right brother redistribute
+// the right has more entries
+void InnerNode::redistributeRight(const int& index, InnerNode* const& rightBro, InnerNode* const& parent) {
+    // TODO
+}
+
+// merge all entries to its left bro, delete this node after merging.
+void InnerNode::mergeLeft(InnerNode* const& leftBro, const Key& k) {
+    // TODO
+}
+
+// merge all entries to its right bro, delete this node after merging.
+void InnerNode::mergeRight(InnerNode* const& rightBro, const Key& k) {
+    // TODO
+}
+
+// remove a children from the current node, used by remove func
+void InnerNode::removeChild(const int& keyIdx, const int& childIdx) {
+    // TODO
+}
+
+// update the target entry, return true if the update succeed.
+bool InnerNode::update(const Key& k, const Value& v) {
+    // TODO
+    if(nKeys == 0 && nChild == 0) return false;
+    int idx = find(k);
+    return childrens[idx]->update(k,v);
+}
+
+// find the target value with the search key, return MAX_VALUE if it fails.
+Value InnerNode::find(const Key& k) {
+    // TODO
+    int place = findIndex(k);
+    return childrens[place]->find(k);
+}
+
+// get the children node of this InnerNode
+Node* InnerNode::getChild(const int& idx) {
+    // TODO
+    if (idx < this->nChild)
+        return this->childrens[idx];
+    return NULL;
+}
+
+// get the key of this InnerNode
+Key InnerNode::getKey(const int& idx) {
+    if (idx < this->nKeys) {
+        return this->keys[idx];
+    } else {
+        return MAX_KEY;
+    }
+}
+
+// print the InnerNode
+void InnerNode::printNode() {
+    cout << "||#|";
+    for (int i = 0; i < this->nKeys; i++) {
+        cout << " " << this->keys[i] << " |#|";
+    }
+    cout << "|" << "    ";
+}
+
+// print the LeafNode
+void LeafNode::printNode() {
+    cout << "||";
+    for (int i = 0; i < 2 * this->degree; i++) {
+        if (this->getBit(i)) {
+            cout << " " << this->kv[i].k << " : " << this->kv[i].v << " |";
+        }
+    }
+    cout << "|" << " ====>> ";
+}
